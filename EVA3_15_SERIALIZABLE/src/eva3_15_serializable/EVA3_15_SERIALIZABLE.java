bdb908dcf397ca9f12308e48c98d3e5de9f4a720
AntonioRamos13 <143569091+AntonioRamos13@users.noreply.github.com>
2024-06-10 11:00:00 -0600

    Add files via upload

---
+++ EVA3_15_SERIALIZABLE/src/eva3_15_serializable/EVA3_15_SERIALIZABLE.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_15_serializable;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA3_15_SERIALIZABLE {
+
+    public static void main(String[] args) {
+        Persona perso= new Persona("David", "Ramos", 13);
+        try {
+            guardarObjeto(perso);
+        } catch (IOException e) {
+        }
+    }
+    public static void guardarObjeto(Persona obj) throws IOException{
+        String ruta="C:\\Users\\selva\\Desktop\\Respaldo_Solver\\archivos\\persona.human";
+        FileOutputStream abrirArch= new FileOutputStream(ruta);
+        ObjectOutputStream guardarArch= new ObjectOutputStream(abrirArch);
+        guardarArch.writeObject(obj);
+        guardarArch.close();
+    }
+   public static Persona leerObjeto() throws FileNotFoundException, IOException, ClassNotFoundException{
+       String ruta= "C:\\Users\\selva\\Desktop\\Respaldo_Solver\\archivos";
+        Persona perso= null;
+        FileInputStream abrirArch= new FileInputStream(ruta);
+        ObjectInputStream leerObj = new ObjectInputStream(abrirArch);
+        perso= (Persona)leerObj.readObject();
+        leerObj.close();
+        return perso;
+        
+    }
+}
