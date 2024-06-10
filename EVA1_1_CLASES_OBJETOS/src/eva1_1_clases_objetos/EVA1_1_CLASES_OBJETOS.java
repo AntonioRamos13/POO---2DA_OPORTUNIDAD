ace1cb4ba98b115b9c5dafb8f055b2d683cc24fb
AntonioRamos13 <143569091+AntonioRamos13@users.noreply.github.com>
2024-06-10 08:13:18 -0600

    Add files via upload

---
+++ EVA1_1_CLASES_OBJETOS/src/eva1_1_clases_objetos/EVA1_1_CLASES_OBJETOS.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package eva1_1_clases_objetos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_1_CLASES_OBJETOS {
+
+    public static void main(String[] args) {
+        
+        Persona perso1 = new Persona(); 
+        System.out.println(perso1);
+    }
+    
+}
+ class Persona{
+ 
+ }
